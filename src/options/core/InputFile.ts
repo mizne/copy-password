d7e067d9b395bee8b3b8d604772e56b1b5a3eb20
mizne <w20054319@126.com>
2017-11-28 00:20:39 +0800

    refactor options.html app.ts and import rxjs for read file

---
+++ src/options/core/InputFile.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs/Observable'
+
+export class InputFile {
+  constructor(private input: HTMLInputElement) {}
+
+  registryChange(): Observable<string | ErrorEvent> {
+    return new Observable((observer) => {
+      this.input.addEventListener('change', function onChange(): void {
+        const reader = new FileReader()
+        const file = this.files[0]
+        reader.onload = function onReaderLoad(): void {
+          observer.next(this.result)
+        }
+        reader.onerror = function onReaderError(ev: ErrorEvent): void {
+          observer.next(ev)
+        }
+        reader.readAsText(file)
+      })
+    })
+  }
+}
--- src/options/options.ts
+++ src/options/options.ts
@@ -1,41 +1,7 @@
 import './options.css'
-// import { App } from './app'
-import Store from '../popup/core/Store'
-import { Prompt } from '../popup/core/Prompt'
+import { App } from './app'
 
 document.addEventListener('DOMContentLoaded', function() {
-  const fileInput = document.querySelector('.file-input') as HTMLInputElement
-  const prompt = document.querySelector('.message') as HTMLElement
-
-  const promptObj = new Prompt(prompt)
-  promptObj.promptInfo(`暂且只支持处理json格式文件！`)
-
-  fileInput.addEventListener('change', function onChange(): void {
-    const reader = new FileReader()
-    const file = this.files[0]
-    reader.onload = function onReaderLoad(): void {
-      try {
-        const obj = JSON.parse(this.result) as { [key: string]: string }
-        const dataItems = Object.keys(obj).map((key, i) => ({
-          id: i,
-          label: key,
-          value: obj[key]
-        }))
-        Store.setDataItems(dataItems)
-        promptObj.promptSuccess(`解析并上传成功！`)
-        window.setTimeout(() => {
-          promptObj.promptInfo(`暂且只支持处理json格式文件！`)
-        }, 2e3)
-      } catch(e) {
-        promptObj.promptError(`解析失败！只支持json格式，上传文件为${file.type}格式！`)
-        window.setTimeout(() => {
-          promptObj.promptInfo(`暂且只支持处理json格式文件！`)
-        }, 5e3)
-      }
-      
-    }
-    reader.readAsText(file)
-  })
-
+  new App().start()
 })
 
